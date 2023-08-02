fac1b4f5e1258f6277962cd7b96aa2b423485885
Kleber Fukushima <kleber.jhun@gmail.com>
2023-08-02 16:32:34 -0300

    Adicionado verificação nome completo

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const form = document.getElementById('form-cadastro');
 const corpoTabela = document.querySelector('tbody');
+const inputNomeCadastro = document.getElementById('nome-cadastro');
+const inputTelCadastro = document.getElementById('tel-cadastro');
 const nomes = [];
 const telefones = [];
+
 let write = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    let nomeValido = false
 
+    nomeValido = validaNome(inputNomeCadastro.value)
+    if (nomeValido) {
     adicionaCadastro();
+    } else {
+        alert('O nome não está completo')
+    }
 
     inputNomeCadastro.value = '';
     inputTelCadastro.value = '';
@@ -15,9 +24,6 @@ form.addEventListener('submit', function(e){
 });
 
 function adicionaCadastro() {
-    const inputNomeCadastro = document.getElementById('nome-cadastro');
-    const inputTelCadastro = document.getElementById('tel-cadastro');
-
     if(nomes.includes(inputNomeCadastro.value) || telefones.includes(inputTelCadastro.value)) {
         alert (`O nome ou o telefone já foi cadastrado`);
     } else {
@@ -27,4 +33,9 @@ function adicionaCadastro() {
     write += linha;
     corpoTabela.innerHTML = write;
 }};
+
+function validaNome(inputNomeCadastro) {
+    const nomeCompletoArray = inputNomeCadastro.split(' ');
+    return nomeCompletoArray.length >= 2
+}
 
